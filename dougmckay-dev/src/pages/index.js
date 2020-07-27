e72e42faf81c237302e691a728c8fc44c4110510
Doug McKay <douglasamckay@gmail.com>
2020-07-27 13:47:13 -0600

    sticky header edits

--- dougmckay-dev/src/pages/index.js
+++ dougmckay-dev/src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const SectionTitle = props => (
   <h2
@@ -163,23 +164,23 @@ const IndexPage = () => (
     <SectionTitle>References</SectionTitle>
     <p>
       Kama Solevad, (360) 261-1055,{" "}
-      <Link to="ksolevad@gmail.com">ksolevad@gmail.com</Link>
+      <Link to="mailto:ksolevad@gmail.com">ksolevad@gmail.com</Link>
     </p>
     <p>
       Josh Balleza, (503) 704-8726,{" "}
-      <Link to="joshuaballeza@gmail.com">joshuaballeza@gmail.com</Link>
+      <Link to="mailto:joshuaballeza@gmail.com">joshuaballeza@gmail.com</Link>
     </p>
     <p>
       Rosalea Peters, (503) 593-0110,{" "}
-      <Link to="3petersfamily@gmail.com">3petersfamily@gmail.com</Link>
+      <Link to="mailto:3petersfamily@gmail.com">3petersfamily@gmail.com</Link>
     </p>
     <p>
       Evan Davis, (503) 913-9991,{" "}
-      <Link to="telavir@gmail.com">telavir@gmail.com</Link>
+      <Link to="mailto:telavir@gmail.com">telavir@gmail.com</Link>
     </p>
     <p>
       Joseph Stith, (801) 201-6357,{" "}
-      <Link to="joseph@weomedia.com">joseph@weomedia.com</Link>
+      <Link to="mailto:joseph@weomedia.com">joseph@weomedia.com</Link>
     </p>
   </Layout>
 )
